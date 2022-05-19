ae672e51bc0b803ff682f20045329be1d37e7062
Philip Okanlawon <f.bobo@live.com>
2022-05-18 23:23:19 -0600

    display fetched photos with attributes

---
+++ script.js
@@ -0,0 +1,53 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+// Unsplash API
+const count = 10;
+const apiKey = 'ff6DeNg9sxWfUWLa-dH94FPXVxkCdtIiB2u_POBzOtk';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+// Create Elements for Links & Photos, Add to DOM
+function displayPhotos() {
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => {
+    // Create <a> to link to Unsplash
+    const item = document.createElement('a');
+    setAttributes(item, {
+      href: photo.links.html,
+      target: '_blank',
+    });
+    // Create <img> for photo
+    const img = document.createElement('img');
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: (photo.alt_description == null) ? 'no description' : photo.alt_description,
+      title: (photo.alt_description == null) ? 'no title' : photo.alt_description,
+    });
+    // Put <img> inside <a>, then put both inside imageContainer
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+// Fetch Photos from Unsplash
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch(err) {
+    // catch error
+  }
+}
+
+// On Load
+getPhotos();
